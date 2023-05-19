594742c9d6611063f4a508af266a771611b68764
miki0412 <chanmiki08412@gmail.com>
2023-05-19 20:35:20 +0900

    commit

--- lib/pages/employee_pages/top_page.dart
+++ lib/pages/employee_pages/top_page.dart
@@ -4,15 +4,43 @@ import 'package:intl/intl.dart';
 import 'package:working_app/style.dart';
 import 'package:working_app/pages/employee_pages/custom_drawer.dart';
 import 'package:working_app/pages/employee_pages/dialyreport_page.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class TopPage extends HookConsumerWidget {
   TopPage({super.key});
 
+  double temperature = 0;
+  int humidity = 0;
+  final NumberFormat num = NumberFormat('#.#');
   final DateFormat date = DateFormat('yyyy年MM月dd日');
   final DateFormat time = DateFormat('HH:mm:ss');
+  
+  final weatherProvider = FutureProvider((ref) async{
+    var apiKey = '31293b57d5e0e715a4ca700d2b5fb6b4';
+    var city = 'Myoko';
+    var lang = 'ja';
+    var url = Uri.parse('http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&lang=$lang');
+    var response = await http.get(url);
+
+    if(response.statusCode == 200){
+      var data = jsonDecode(response.body);
+      var weather = data['weather'][0];
+      var main = data['main'];
+
+      return {
+        'weatherDescription' : weather['description'],
+        'temperature' : main['temp'],
+        'humidity' : main['humidity'],
+      };
+    }else{
+      throw Exception('リクエストが失敗しました。ステータスコード${response.statusCode}');
+    }
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final weathers = ref.watch(weatherProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text('トップページ',style: Textstyle.titlesize),
@@ -26,6 +54,18 @@ class TopPage extends HookConsumerWidget {
           children: [
             Text(
                 '山田太郎　ログイン日時${date.format(DateTime.now())}${time.format(DateTime.now())}'),
+            Row(children:[const Text('今日の天気　'),
+            weathers.when(
+              loading: () => const CircularProgressIndicator(),
+              error: (error,stackTrack) => const Text('読み取りに失敗しました'),
+              data: (data) => Row(
+                children: [
+                  Text('天気 : ${data['weatherDescription']}'),
+                  Text(' 温度 : ${num.format(data['temperature']-273.15)}℃'),
+                  Text(' 湿度 : ${num.format(data['humidity'])}％'),
+                ],
+              ),
+            ),],),
             Container(
               alignment: Alignment.center,
               margin: const EdgeInsets.only(top: 150, bottom: 50),
